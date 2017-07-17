e8ee2592732d0f5281a8ff0cbbde81e46656ce2b
dchase87 <david.chase4@gmail.com>
2017-07-17 16:54:53 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,23 @@
-export default function createStore(reducer) {
-  // add your code here
+export default function createStore (reducer) {
+  let state
+
+  function getState () {
+    return state
+  }
+
+  function dispatch (action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  dispatch({ type: '@@INIT' })
+
+  return {
+    getState,
+    dispatch
+  }
 }
 
-function render() {
-  const container = document.getElementById('container');
+function render () {
+  const container = document.getElementById('container')
 }
